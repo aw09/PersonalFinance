07aa4ce71aa4797687a6316e4a1dd2b5bb97149c
Agung Wicaksono <agungwck99@gmail.com>
2025-08-03 17:48:51 +0700

    feat: re init supabase

--- supabase/seed.sql
+++
@@ -1,5 +0,0 @@
--- Seed data for the Personal Finance application
--- Note: Default categories are created per user by the application when they register.
--- This file can be used for other seed data that doesn't depend on specific users.
-
--- No seed data currently needed - all data is user-specific and created by the application
